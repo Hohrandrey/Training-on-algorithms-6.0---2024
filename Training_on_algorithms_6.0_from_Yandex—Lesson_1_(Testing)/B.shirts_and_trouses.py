546874e29e991a9fc76917fa8d203e0edbefe619
Hohrandrey <EnderBro_2005@mail.ru>
2024-10-27 17:59:37 +0300

    Create B.shirts_and_trouses.py

---
+++ Training_on_algorithms_6.0_from_Yandex—Lesson_1_(Testing)/B.shirts_and_trouses.py
@@ -0,0 +1,12 @@
+# Чтение данных
+A = int(input())
+B = int(input())
+C = int(input())
+D = int(input())
+
+# Вычисление количества маек и носков
+M = min(A + 1, B+1)
+N = min(C + 1, D+1)
+
+# Вывод результата
+print(M, N)
