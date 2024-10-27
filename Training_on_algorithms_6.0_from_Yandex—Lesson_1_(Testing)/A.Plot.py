dfb6b54ca90f31c195024585aac0611636ebc0fc
Hohrandrey <EnderBro_2005@mail.ru>
2024-10-27 17:59:35 +0300

    Update A.Plot.py

--- Training_on_algorithms_6.0_from_Yandex—Lesson_1_(Testing)/A.Plot.py
+++ Training_on_algorithms_6.0_from_Yandex—Lesson_1_(Testing)/A.Plot.py
@@ -1,12 +1,12 @@
 #Левый нижний угол
-x1 = int(input('Введите x1: '))
-y1 = int(input('Введите y1: '))
+x1 = int(input(''))
+y1 = int(input(''))
 #Правый верхний угол
-x2 = int(input('Введите x2: '))
-y2 = int(input('Введите y2: '))
+x2 = int(input(''))
+y2 = int(input(''))
 #Чел
-x = int(input('Введите x: '))
-y = int(input('Введите y: '))
+x = int(input(''))
+y = int(input(''))
 
 
 def direction_to_swimmer(x1, y1, x2, y2, x, y):
